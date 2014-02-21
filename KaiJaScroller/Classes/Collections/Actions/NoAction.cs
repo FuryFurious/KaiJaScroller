0d769f2212f4ea8efc73f32d5755c64026ff6991
FuryFurious <ricu05@yahoo.de>
2014-02-21 20:36:23 +0100

    - added bullets

---
+++ KaiJaScroller/Classes/Collections/Actions/NoAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class NoAction : IAction
+{
+    public bool performed(GameTime gameTime, InGame ingame)
+    {
+        return false;
+    }
+}
