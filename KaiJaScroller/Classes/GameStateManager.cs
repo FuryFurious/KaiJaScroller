26842d03d2c4c667fc159cdd701bb51b4540a44f
FuryFurious <ricu05@yahoo.de>
2014-02-21 10:41:20 +0100

    - First code base

---
+++ KaiJaScroller/Classes/GameStateManager.cs
@@ -0,0 +1,27 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class GameStateManager : Game
+{
+
+    public GameStateManager()
+        : base(800, 600, "Title", SFML.Window.Styles.Default)
+    {
+
+    }
+
+    public override void update(GameTime gameTime)
+    {
+
+    }
+
+    public override void draw(GameTime gameTime, RenderWindow window)
+    {
+
+    }
+}
---
+++ KaiJaScroller/Classes/GameTemplate/Game.cs
@@ -0,0 +1,70 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Game
+{
+
+    public static readonly Color CornflowerBlue = new Color(101, 156, 239);
+
+    public RenderWindow     window;
+    public GameTime         gameTime; 
+
+    public static int wheelDelta;
+
+    public Game(int width, int height, String title, Styles style)
+    {
+        window = new RenderWindow(new VideoMode((uint)width, (uint)height), title, style);
+
+        window.SetMouseCursorVisible(true);
+
+        window.Closed += closeHandler;
+        window.MouseWheelMoved += mouseWheelHandler;
+        
+        window.SetVerticalSyncEnabled(true);
+        window.SetFramerateLimit(60);
+
+        gameTime = new GameTime();
+    }
+
+    private void mouseWheelHandler(object sender, MouseWheelEventArgs e)
+    {
+        wheelDelta = e.Delta;
+    }
+
+    private void closeHandler(object sender, EventArgs e)
+    {
+        window.Close();
+    }
+
+    public void run()
+    {
+        gameTime.Start();
+
+        while (window.IsOpen())
+        {
+            window.DispatchEvents();
+
+        
+            gameTime.Update();
+
+            update(gameTime);
+            draw(gameTime, window);
+
+            wheelDelta = 0;
+            window.Display();
+        }
+    }
+    
+    public abstract void update(GameTime gameTime);
+
+    public abstract void draw(GameTime gameTime, RenderWindow window);
+
+
+
+}
+
