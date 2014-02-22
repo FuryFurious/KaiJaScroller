0c0c2aa764717f078e3522cbc17dd663d744774b
FuryFurious <ricu05@yahoo.de>
2014-02-22 01:51:42 +0100

    - fixed wrong directories

--- KaiJaScroller/KaiJaScroller/Classes/Statics/Enums.cs
+++
@@ -1,7 +0,0 @@
-﻿
-
-
-public enum EGameState { None, MainMenu, InGame, Credits, Restart, GameStateCount };
-public enum EDirection { Left, Right, DirectionCount }
-public enum EEntityType {None, Player };
-
