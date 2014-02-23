2af48b2a3bef208fdaf40ff492552c00133455ce
Hawkcutter <jarek.business@googlemail.com>
2014-02-23 10:08:31 +0100

    ccc

--- KaiJaScroller/Classes/Components/Behavior/Entities/ChaseBrain.cs
+++ KaiJaScroller/Classes/Components/Behavior/Entities/ChaseBrain.cs
@@ -8,10 +8,7 @@ using System.Threading.Tasks;
 public class ChaseBrain : ABehavior
 {
 
-    float pX;
-    float eX;
-    float pY;
-    float eY;
+    
 
     public ChaseBrain()
     {
@@ -25,16 +22,13 @@ public class ChaseBrain : ABehavior
         //skill1.setEntity(this.entity);
 
         //skills.Add(skill1);
-         pX = this.entity.ingame.player.boundingBox.CenterX;
-    eX = this.entity.boundingBox.CenterX;
-    pY = this.entity.ingame.player.boundingBox.CenterY;
-    eY = this.entity.boundingBox.CenterY;
+         
         
     }
 
     public override void update(GameTime gameTime)
     {
-        if (pX - eX < -5 && pX - eX > -150 && this.entity.canMoveLeft(2, 0))
+        if (this.entity.ingame.player.position.X - this.entity.position.X < -5 && this.entity.ingame.player.position.X - this.entity.position.X > -150 && this.entity.canMoveLeft(2, 0))
             this.entity.moveLeft(2);
 
         else if (this.entity.ingame.player.position.X - this.entity.position.X > 5 && this.entity.ingame.player.position.X - this.entity.position.X < 150 && this.entity.canMoveRight(2, 0))
